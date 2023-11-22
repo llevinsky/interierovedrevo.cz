22cb6fd6053950f51003b3a11a968a6f2c6134de
llevinsky <135062573+llevinsky@users.noreply.github.com>
2023-11-22 09:55:46 +0100

    drobné úpravy

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
+// Skript pro interaktivitu navigačního dropdown menu
+
 document.addEventListener('DOMContentLoaded', function () {
-    // Get the toggle button and dropdown content
+    // Získání odkazu na tlačítko pro otevření dropdown menu a obsahu dropdown menu
     const toggleButton = document.getElementById('dropdownToggle');
     const dropdownContent = document.querySelector('.dropdown-content');
 
-    // Add a click event listener to the toggle button
+    // Přidání posluchače událostí pro kliknutí na tlačítko
     toggleButton.addEventListener('click', function () {
-        // Toggle the visibility of the dropdown content
+        // Přepnutí viditelnosti obsahu dropdown menu
         dropdownContent.classList.toggle('show');
     });
 
-    // Get the close button
+    // Získání odkazu na tlačítko pro zavření dropdown menu
     const closeButton = document.getElementById('closeButton');
 
-    // Add a click event listener to the close button
+    // Přidání posluchače událostí pro kliknutí na tlačítko pro zavření
     closeButton.addEventListener('click', function () {
-        // Hide the dropdown content
+        // Skrytí obsahu dropdown menu
         dropdownContent.classList.remove('show');
     });
 });
